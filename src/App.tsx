f012e2eb0bac53008ac66acbfd1f5e6457ed194f
JuhaniVuorinen <jjvuorinen05@gmail.com>
2022-11-10 13:43:17 +0200

    Installed SASS

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 // App.tsx
+import SomeExampleComponent from './components/example/someExampleComponent'
+
 import React from 'react'
 import MyFirstComponent from './components/myFirstComponent';
 
 const App: React.FC = () => {
   return (
     <div>
-      <MyFirstComponent />
+      <SomeExampleComponent />
     </div>
   )
 }
